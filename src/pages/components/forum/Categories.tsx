6e71e8cf77fb3652c09e4fdb633b25e079bd1ef6
Xavimc04 <xaviermorcam@campus.monlau.com>
2023-07-18 20:29:16 +0200

    Forum posts animated.

--- src/pages/components/forum/Categories.tsx
+++ src/pages/components/forum/Categories.tsx
@@ -2,6 +2,7 @@ import { ForumCategory } from "@prisma/client";
 import { useContext, useState } from "react"; 
 import { AuthContext } from "@/providers/Auth.context";
 import ContextMenu, { ContextCoords } from "./ContextMenu";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Categories() {  
     const { state, dispatch } : any = useContext(AuthContext);  
@@ -23,44 +24,73 @@ export default function Categories() {
         handleContextDisplay(true); 
     }
 
-    return <div className="mx-10 min-w-[300px] max-w-[500px] flex flex-col gap-5">
-        <div className="flex items-center mb-5 opacity-60 select-none cursor-pointer">
+    return <AnimatePresence>
+        <div className="mx-10 min-w-[300px] max-w-[500px] flex flex-col gap-5">
+            <motion.div
+                className="flex items-center mb-5 opacity-60 select-none cursor-pointer"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+            >
                 <span onClick={() => dispatch({
                     type: "SET_CATEGORY_ROUTE", 
                     payload: []
                 })}>Inicio /</span>
 
                 {
-                    state.categoryRoute && state.categoryRoute[0] && state.categoryRoute.map((route:any, index:number) => {
-                        return <span className="pl-2" key={ index }>{ route.name } /</span>
+                    state.categoryRoute &&
+                    state.categoryRoute.map((route: any, index: number) => {
+                        return (
+                        <motion.span
+                            className="pl-2"
+                            key={index}
+                            initial={{ opacity: 0, y: -10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -10 }}
+                        >
+                            { route.name } /
+                        </motion.span>
+                        );
                     })
                 }
-            </div> 
+            </motion.div> 
 
-        {
-            state.categories && state.categories.length > 0 ? state.categories.filter((category: ForumCategory) => {
-                if (state.categoryRoute.length === 0) { 
-                    return category.parentId === null;
-                } else { 
-                    const lastRouteItem = state.categoryRoute[state.categoryRoute.length - 1]; 
+            {
+                state.categories && state.categories.length > 0 ? ( 
+                    state.categories.filter((category: ForumCategory) => {
+                        if (state.categoryRoute.length === 0) {
+                            return category.parentId === null;
+                        } else {
+                            const lastRouteItem =
+                            state.categoryRoute[state.categoryRoute.length - 1];
+                            return category.parentId === lastRouteItem.id;
+                        }
+                    }).map((category: ForumCategory) => {
+                        return (
+                            <motion.div 
+                                key={category.id}
+                                onClick={() =>
+                                    dispatch({
+                                    type: "SET_CATEGORY_ROUTE",
+                                    payload: [...state.categoryRoute, category],
+                                    })
+                                }
+                                className="select-none cursor-pointer hover:text-violet-500 transition-colors"
+                                onContextMenu={(e) => handleContext(e, category.id)}
+                                initial={{ opacity: 0, y: -10 }}
+                                animate={{ opacity: 1, y: 0 }} 
+                                exit={{ opacity: 0, y: -10 }} 
+                            >
+                            {category.name}
+                            </motion.div>
+                        );
+                    })
+                ) : (
+                <div>Sin categorías registradas</div>
+            )}
 
-                    return category.parentId === lastRouteItem.id;
-                }
-            }).map((category:ForumCategory) => {
-                return <div key={ category.id } onClick={() => 
-                    dispatch({
-                        type: "SET_CATEGORY_ROUTE", 
-                        payload: [
-                        ...state.categoryRoute, category
-                    ]
-                })} className="select-none cursor-pointer hover:text-violet-500 transition-colors"
-                    onContextMenu={(e) => handleContext(e, category.id) }
-                >
-                    { category.name }
-                </div>
-            }) : 'Sin categorías registradas'
-        }
 
-        <ContextMenu display={ contextDisplay } coords={ contextCoords } setDisplay={ handleContextDisplay } categoryId={ selectedId } />
-    </div>
+            <ContextMenu display={ contextDisplay } coords={ contextCoords } setDisplay={ handleContextDisplay } categoryId={ selectedId } />
+        </div>
+    </AnimatePresence>
 }
--- src/pages/components/forum/RenderPosts.tsx
+++ src/pages/components/forum/RenderPosts.tsx
@@ -1,6 +1,7 @@
 import { AuthContext } from "@/providers/Auth.context";
 import { getPosts } from "@/services/forumService";
 import { Post } from "@prisma/client";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState, useEffect, useContext } from "react";
 
 const STAKE = 10; 
@@ -55,45 +56,55 @@ export default function RenderPosts() {
         return number.toString();
     };
 
-    return <div className="w-full flex flex-col gap-5">
-        <div className="w-full text-end text-gray-400">
-            Mostrando { posts.length } de { totalCounter } resultados...
-        </div>
+    return <AnimatePresence>
+        <div className="w-full flex flex-col gap-5">
+            <div className="w-full text-end text-gray-400">
+                Mostrando { posts.length } de { totalCounter } resultados...
+            </div>
 
-        {
-            posts && posts[0] && posts.map((post: Post) => {
-                return <div key={ Math.floor(Math.random() * 100000) } className="w-full bg-slate-900 rounded flex items-center py-5">
-                    <span className="text-gray-600 px-10 text-lg">
-                        # { post.id }
-                    </span>
-
-                    <div className="flex flex-col flex-1 ">
-                        <h2 className="text-xl">{ post.title && (
-                            post.title.slice(0, 30) + (
-                                post.title.length > 30 ? '...' : ''
-                            )  
-                        ) }</h2>
-
-                        { state.categories.filter((category: any) => category.id == post.category).map((category:any) => {
-                            return <small key={ Math.floor(Math.random() * 100000) } className="text-gray-600">{ category.name }</small>
-                        }) }
-                    </div>
-
-                    <div className="flex items-center gap-5 mr-10">  
-                        {
-                            post.pinned && <span className="material-symbols-outlined text-4xl text-green-500">stat_2</span>
-                        }
-
-                        <span className="text-gray-600">
-                            { formatNumber(post.views || 0) } 
+            {
+                posts && posts[0] && posts.map((post: Post) => {
+                    return <motion.div
+                        key={Math.floor(Math.random() * 100000)}
+                        className="w-full bg-slate-900 rounded flex items-center py-5"
+                        initial={{ opacity: 0, y: 50 }} 
+                        animate={{ opacity: 1, y: 0 }} 
+                        exit={{ opacity: 0, y: -50 }} 
+                    >
+                        <span className="text-gray-600 px-10 text-lg">
+                            # { post.id }
                         </span>
-                    </div>
-                </div>
-            }) 
-        }
 
-        {
-            (SKIP_VALUE + STAKE) < totalCounter && <button onClick={() => setSkipValue(SKIP_VALUE + STAKE)}>Cargar más...</button>
-        }
-    </div>
+                        <div className="flex flex-col flex-1 ">
+                            <h2 className="text-xl">{ post.title && (
+                                post.title.slice(0, 30) + (
+                                    post.title.length > 30 ? '...' : ''
+                                )  
+                            ) }</h2>
+
+                            { 
+                                state.categories.filter((category: any) => category.id == post.category).map((category:any) => {
+                                    return <small key={ Math.floor(Math.random() * 100000) } className="text-gray-600">{ category.name }</small>
+                                }) 
+                            }
+                        </div>
+
+                        <div className="flex items-center gap-5 mr-10">  
+                            {
+                                post.pinned && <span className="material-symbols-outlined text-4xl text-green-500">stat_2</span>
+                            }
+
+                            <span className="text-gray-600">
+                                { formatNumber(post.views || 0) } 
+                            </span>
+                        </div>
+                    </motion.div>
+                }) 
+            }
+
+            {
+                (SKIP_VALUE + STAKE) < totalCounter && <button onClick={() => setSkipValue(SKIP_VALUE + STAKE)}>Cargar más...</button>
+            }
+        </div>
+    </AnimatePresence>
 }
